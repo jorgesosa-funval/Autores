b5e29b249ff8825c373ba89ba4284210792f5d74
unknown <jorgesosa@fundaciondevalores.org>
2024-03-05 17:01:15 -0400

    initial commit

---
+++ scripts/data.js
@@ -0,0 +1,249 @@
+export const autores = [
+  {
+    id: 1,
+    nombreAutor: "Gabriel García Márquez",
+    obras: [
+      {
+        id: 1,
+        titulo: "Cien años de soledad",
+        añoPublicacion: 1967,
+        genero: "Realismo mágico",
+        portada: "https://wmagazin.com/relatos/cien-anos-de-soledad-en-todo-el-mundo-sus-portadas-en-20-idiomas/",
+      },
+      {
+        id: 2,
+        titulo: "El amor en los tiempos del cólera",
+        añoPublicacion: 1985,
+        genero: "Realismo mágico",
+        portada: "https://www.pinterest.com/ivy0489/el-amor-en-los-tiempos-del-c%C3%B3lera/",
+      },
+      {
+        id: 3,
+        titulo: "El coronel no tiene quien le escriba",
+        añoPublicacion: 1961,
+        genero: "Realismo mágico",
+        portada: "https://aldeadeperiodistas.com/el-coronel-no-tiene-quien-le-escriba-fragmento/el-coronel/",
+      },
+    ],
+  },
+  {
+    id: 2,
+    nombreAutor: "Mario Vargas Llosa",
+    obras: [
+      {
+        id: 4,
+        titulo: "La ciudad y los perros",
+        añoPublicacion: 1962,
+        genero: "Novela de aprendizaje",
+        portada: "https://blogs.uoregon.edu/lcylp/2015/07/27/las-portadas-y-los-perros/",
+      },
+      {
+        id: 5,
+        titulo: "La casa verde",
+        añoPublicacion: 1966,
+        genero: "Realismo",
+        portada: "https://www.pinterest.com/pin/22025485671274310/",
+      },
+      {
+        id: 6,
+        titulo: "Conversación en la Catedral",
+        añoPublicacion: 1969,
+        genero: "Novela política",
+        portada: "https://www.researchgate.net/figure/Figura-1-Portada-de-la-novela-Conversacion-en-La-Catedral-de-Mario-Vargas-Llosa-Fuente_fig1_342987749",
+      },
+    ],
+  },
+  {
+    id: 3,
+    nombreAutor: "Jorge Luis Borges",
+    obras: [
+      {
+        id: 7,
+        titulo: "Ficciones",
+        añoPublicacion: 1944,
+        genero: "Cuento",
+        portada: "https://en.wikipedia.org/wiki/Ficciones",
+      },
+      {
+        id: 8,
+        titulo: "El Aleph",
+        añoPublicacion: 1949,
+        genero: "Cuento",
+        portada: "https://www.amazon.com/El-Aleph-Jorge-Luis-Borges/dp/846634683X",
+      },
+      {
+        id: 9,
+        titulo: "El Hacedor",
+        añoPublicacion: 1960,
+        genero: "Poesía",
+        portada: "https://www.literatura.us/borges/hacedor.html",
+      },
+    ],
+  },
+  {
+    id: 4,
+    nombreAutor: "Isabel Allende",
+    obras: [
+      {
+        id: 10,
+        titulo: "La casa de los espíritus",
+        añoPublicacion: 1982,
+        genero: "Realismo mágico",
+        portada: "https://www.memoriachilena.gob.cl/602/w3-article-65106.html",
+      },
+      {
+        id: 11,
+        titulo: "De amor y de sombra",
+        añoPublicacion: 1984,
+        genero: "Realismo mágico",
+        portada: "https://www.memoriachilena.gob.cl/602/w3-article-65112.html",
+      },
+      {
+        id: 12,
+        titulo: "Eva Luna",
+        añoPublicacion: 1987,
+        genero: "Realismo mágico",
+        portada: "https://www.instagram.com/evaluna/",
+      },
+    ],
+  },
+  {
+    id: 5,
+    nombreAutor: "Miguel de Cervantes",
+    obras: [
+      {
+        id: 13,
+        titulo: "Don Quijote de la Mancha",
+        añoPublicacion: 1605,
+        genero: "Novela picaresca",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Don_Quijote_de_la_Mancha_1605.jpg/1200px-Don_Quijote_de_la_Mancha_1605.jpg",
+      },
+    ],
+  },
+  {
+    id: 6,
+    nombreAutor: "William Shakespeare",
+    obras: [
+      {
+        id: 14,
+        titulo: "Hamlet",
+        añoPublicacion: 1603,
+        genero: "Tragedia",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Hamlet_1603.jpg/1200px-Hamlet_1603.jpg",
+      },
+      {
+        id: 15,
+        titulo: "Romeo y Julieta",
+        añoPublicacion: 1597,
+        genero: "Tragedia",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/Romeo_and_Juliet_1597.jpg/1200px-Romeo_and_Juliet_1597.jpg",
+      },
+    ],
+  },
+  {
+    id: 7,
+    nombreAutor: "Jane Austen",
+    obras: [
+      {
+        id: 16,
+        titulo: "Orgullo y prejuicio",
+        añoPublicacion: 1813,
+        genero: "Novela romántica",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a3/Pride_and_Prejudice_1813.jpg/1200px-Pride_and_Prejudice_1813.jpg",
+      },
+    ],
+  },
+  {
+    id: 8,
+    nombreAutor: "Franz Kafka",
+    obras: [
+      {
+        id: 17,
+        titulo: "La metamorfosis",
+        añoPublicacion: 1915,
+        genero: "Novela corta",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a8/Die_Verwandlung_1915.jpg/1200px-Die_Verwandlung_1915.jpg",
+      },
+    ],
+  },
+  {
+    id: 9,
+    nombreAutor: "Julio Cortázar",
+    obras: [
+      {
+        id: 18,
+        titulo: "Rayuela",
+        añoPublicacion: 1963,
+        genero: "Novela",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Rayuela_1963.jpg/1200px-Rayuela_1963.jpg",
+      },
+      {
+        id: 19,
+        titulo: "Historias de cronopios y de famas",
+        añoPublicacion: 1962,
+        genero: "Cuentos",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/Historias_de_cronopios_y_de_famas_1962.jpg/1200px-Historias_de_cronopios_y_de_famas_1962.jpg",
+      },
+    ],
+  },
+  {
+    id: 10,
+    nombreAutor: "George Orwell",
+    obras: [
+      {
+        id: 21,
+        titulo: "1984",
+        añoPublicacion: 1949,
+        genero: "Novela distópica",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c4/Nineteen_Eighty-Four_first_edition.jpg/1200px-Nineteen_Eighty-Four_first_edition.jpg",
+      },
+      {
+        id: 22,
+        titulo: "Rebelión en la granja",
+        añoPublicacion: 1945,
+        genero: "Fábula política",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Animal_Farm_first_edition.jpg/1200px-Animal_Farm_first_edition.jpg",
+      },
+    ],
+  },
+  {
+    id: 14,
+    nombreAutor: "Agatha Christie",
+    obras: [
+      {
+        id: 28,
+        titulo: "The Murder on the Orient Express",
+        añoPublicacion: 1934,
+        genero: "Novela policíaca",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/Murder_on_the_Orient_Express_%281934%29.jpg/1200px-Murder_on_the_Orient_Express_%281934%29.jpg",
+      },
+      {
+        id: 29,
+        titulo: "And Then There Were None",
+        añoPublicacion: 1939,
+        genero: "Novela policíaca",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6c/And_Then_There_Were_None_%281939%29.jpg/1200px-And_Then_There_Were_None_%29.jpg",
+      },
+    ],
+  },
+  {
+    id: 12,
+    nombreAutor: "Federico García Lorca",
+    obras: [
+      {
+        id: 24,
+        titulo: "Romancero gitano",
+        añoPublicacion: 1928,
+        genero: "Poesía",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Romancero_gitano_%28primera_edici%C3%B3n%29.jpg/1200px-Romancero_gitano_%28primera_edici%C3%B3n%29.jpg",
+      },
+      {
+        id: 25,
+        titulo: "Bodas de sangre",
+        añoPublicacion: 1932,
+        genero: "Tragedia",
+        portada: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/54/Bodas_de_sangre_%28primera_edici%C3%B3n%29.jpg/1200px-Bodas_de_sangre_%28primera_edici%C3%B3n%29.jpg",
+      },
+    ],
+  },
+];
---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+ import { autores } from "./data.js";
+  
+ /* 1: En la variable autores tiens una lista de  diferentes autores, filtra la lista y crea un nuevo array donde almacenes los nombres de cada autor y su id  eje: [{id: x , autor: nombre_autor}]*/
