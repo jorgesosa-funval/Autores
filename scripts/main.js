a0413056f8d2fe24db9b32022a946158cbe6b28c
unknown <jorgesosa@fundaciondevalores.org>
2024-03-06 10:02:49 -0400

    nuevas instrucciones

--- scripts/main.js
+++ scripts/main.js
@@ -1,23 +1,33 @@
  import { autores } from "./data.js";
   
- /* 
  
-    1: En la variable autores tienes una lista de  diferentes autores, filtra la lista y crea un nuevo array donde almacenes los nombres de cada autor y su id  ejem: [{id: x , autor: nombre_autor}]
+ /* 
+Instrucciones mejoradas:
 
-    2: Crea una funcion nombreDeTuPreferencia que recibira como parametros dos valores (id, nombre)
-    
-    3: Dentro de la funcion que creaste anteriormente:
-         0: crea un elemento li  
-         1: agrega un id al elemento (el id lo estas recibiendo como paramentro) ----> li.id = id 
-         2: agrega la clase item_autor ----> li.classList.add('item_autor');
-         3: agrega contenido al li  (el contenido es el nombre que estas  recibiendo como parametro)
+1. Filtrar y crear un nuevo array:
+   - En la variable "autores", tienes una lista de diferentes autores. Filtra la lista y crea un nuevo array donde almacenes los nombres de cada autor y su id, por ejemplo: [{id: x, autor: nombre_autor}]
 
-    4:  En el html encontraras una lista (ul) agregale un id: 
-        0: utiliza getElementById o querySelector para llamar el elemento con el id que acabas de agregar.
-        1: a este elemento agregale el li que creaste anteriormente. appendChield(li) 
-    
-    5: itera la lista de Usuarios que creaste en el punto 1:
-       0: llama la funcion que craste en el punto 3 dentro de la iteracion que acabas de crear
-       2: del item que te devuelve el iterador facilita los parametros que espera la funcion.
+2. Crear una función "nombreDeTuPreferencia":
+   - Esta función recibirá como parámetros dos valores (id, nombre).
 
- */
+3. Dentro de la función creada anteriormente:
+   0. Crea un elemento li.
+   1. Agrega un id al elemento (el id lo estás recibiendo como parámetro) --> li.id = id.
+   2. Agrega la clase "item_autor" --> li.classList.add('item_autor').
+   3. Agrega contenido al li (el contenido es el nombre que estás recibiendo como parámetro).
+
+4. En el HTML, encontrarás una lista (ul). Agregále un id:
+   0. Utiliza getElementById o querySelector para llamar el elemento con el id que acabas de agregar.
+   1. A este elemento, agrégale el li que creaste anteriormente mediante appendChild(li).
+
+5. Itera la lista de usuarios que creaste en el punto 1:
+   0. Llama a la función que creaste en el punto 3 dentro de la iteración recién creada.
+   1. Del item que te devuelve el iterador, facilita los parámetros que espera la función.
+
+6. Al li que creaste anteriormente, agrega un escuchador de evento click (.addEventListener)
+   0. Obtén el id del li al cual le hagas click y muéstralo con console.log() o alert();
+
+7. Filtrar y crear un nuevo array:
+    0. En cada objeto del array autores existe una llave llamada *obras:* esta es un array de objetos. De cada uno de los objetos dentro de obras, obtén el género de cada obra y agrégalos a un nuevo array.   
+    1. Si haces un console.log() del array que creaste anteriormente notarás que hay valores repetidos, evita que hayan valores repetidos.
+*/
